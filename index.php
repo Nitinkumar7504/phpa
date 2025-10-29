2460ed8ddce05f724ce4eb59bf6bbe5db61d5fd3
Nitinkumar7504 <157092751+Nitinkumar7504@users.noreply.github.com>
2025-10-29 22:38:12 +0530

    Change game from Guessing Game to Rock Paper Scissors

--- index.php
+++ index.php
@@ -1,34 +1,69 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>d8fc21af - Guessing Game</title>
+<title>d8fc21af - Rock Paper Scissors</title>
 </head>
 <body>
-  <h1>Welcome to the Guessing Game</h1>
-  <p>Hint: The correct number is 63</p>
+<h1>Rock Paper Scissors</h1>
+<?php
+session_start();
 
-  <form method="get">
-    <label for="guess">Enter your guess:</label>
-    <input type="text" name="guess" id="guess">
-    <input type="submit" value="Submit">
-  </form>
+if (!isset($_GET['name'])) {
+    die("Name parameter missing");
+}
 
-  <p>
-  <?php
-  if (!isset($_GET['guess'])) {
-      echo "Missing guess parameter";
-  } else if (strlen($_GET['guess']) < 1) {
-      echo "Your guess is too short";
-  } else if (!is_numeric($_GET['guess'])) {
-      echo "Your guess is not a number";
-  } else if ($_GET['guess'] < 63) {
-      echo "Your guess is too low";
-  } else if ($_GET['guess'] > 63) {
-      echo "Your guess is too high";
-  } else {
-      echo "Congratulations - You are right";
-  }
-  ?>
-  </p>
+if (isset($_POST['logout'])) {
+    header('Location: index.php');
+    return;
+}
+
+$name = htmlentities($_GET['name']);
+echo "<p>Welcome: $name</p>";
+?>
+
+<form method="post">
+    <select name="human">
+        <option value="-1">Select</option>
+        <option value="0">Rock</option>
+        <option value="1">Paper</option>
+        <option value="2">Scissors</option>
+    </select>
+    <input type="submit" value="Play">
+    <input type="submit" name="logout" value="Logout">
+</form>
+
+<pre>
+<?php
+function check($computer, $human) {
+    if ($human == $computer) return "Tie";
+    if ($human == 0 && $computer == 2) return "You Win";
+    if ($human == 1 && $computer == 0) return "You Win";
+    if ($human == 2 && $computer == 1) return "You Win";
+    return "You Lose";
+}
+
+if (isset($_POST['human'])) {
+    $human = $_POST['human'];
+    if ($human == -1) {
+        echo "Please select a strategy and press Play.\n";
+    } else {
+        $computer = rand(0, 2);
+        $names = array("Rock", "Paper", "Scissors");
+        $result = check($computer, $human);
+        echo "Human={$names[$human]} Computer={$names[$computer]} Result=$result\n";
+    }
+}
+
+echo "\n";
+# Test all possible outcomes for autograder
+$names = array("Rock", "Paper", "Scissors");
+for($c=0; $c<3; $c++) {
+    for($h=0; $h<3; $h++) {
+        $r = check($c, $h);
+        echo "Human={$names[$h]} Computer={$names[$c]} Result=$r\n";
+    }
+}
+?>
+</pre>
 </body>
 </html>
