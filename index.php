7675afdd21a23fcc5204e881703ef716c60c212d
Nitinkumar7504 <157092751+Nitinkumar7504@users.noreply.github.com>
2025-10-29 22:30:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
   <title>d8fc21af - Guessing Game</title>
-  <meta charset="utf-8" />
 </head>
 <body>
-  <h1>Welcome DAKSH RAINA</h1>
-  <p>Guess a number between 1 and 100</p>
+  <h1>Welcome to the Guessing Game</h1>
+  <p>Hint: The correct number is 63</p>
 
-  <form method="get" action="guess.php">
-    <input type="text" name="guess" />
-    <input type="submit" value="Guess" />
+  <form method="get">
+    <label for="guess">Enter your guess:</label>
+    <input type="text" name="guess" id="guess">
+    <input type="submit" value="Submit">
   </form>
 
-  <p>Hint: this assignment expects the correct answer to be 63.</p>
+  <p>
+  <?php
+  if (!isset($_GET['guess'])) {
+      echo "Missing guess parameter";
+  } else if (strlen($_GET['guess']) < 1) {
+      echo "Your guess is too short";
+  } else if (!is_numeric($_GET['guess'])) {
+      echo "Your guess is not a number";
+  } else if ($_GET['guess'] < 63) {
+      echo "Your guess is too low";
+  } else if ($_GET['guess'] > 63) {
+      echo "Your guess is too high";
+  } else {
+      echo "Congratulations - You are right";
+  }
+  ?>
+  </p>
 </body>
 </html>
