a149d97108c671ce316dff6708db8c4bf1b5d00f
Nitinkumar7504 <157092751+Nitinkumar7504@users.noreply.github.com>
2025-10-29 21:58:21 +0530

    Create index.php for guessing game

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <title>d8fc21af - Guessing Game</title>
+  <meta charset="utf-8" />
+</head>
+<body>
+  <h1>Welcome DAKSH RAINA</h1>
+  <p>Guess a number between 1 and 100</p>
+
+  <form method="get" action="guess.php">
+    <input type="text" name="guess" />
+    <input type="submit" value="Guess" />
+  </form>
+
+  <p>Hint: this assignment expects the correct answer to be 63.</p>
+</body>
+</html>
